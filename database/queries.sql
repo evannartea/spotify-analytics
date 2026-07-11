d7ab3caa3b30be2292866de9687575832d04d459
Evan Nartea <evannartea@Mac.lan>
2026-07-11 17:12:18 +0100

    add artist dim table

--- database/queries.sql
+++ database/queries.sql
@@ -1,7 +1,10 @@
+-- STAGING
 -- Filter raw data
+DROP TABLE IF EXISTS staging.spotify_streams;
+
 CREATE TABLE staging.spotify_streams AS
 SELECT 
-	ts AS played_at, 
+	ts AS date_played, 
 	ms_played AS milliseconds_played, 
 	conn_country AS country, 
 	master_metadata_track_name AS track_name, 
@@ -16,4 +19,18 @@ SELECT
 	mins_played
 FROM public.streaming_history
 ORDER BY ts ASC;
+
+-- WARHOUSE
+-- Create artist dim
+DROP TABLE IF EXISTS warehouse.dim_artist;
+
+CREATE TABLE warehouse.dim_artist AS
+SELECT
+	ROW_NUMBER() OVER(ORDER BY artist_name) AS artist_key,
+	artist_name
+FROM (
+	SELECT DISTINCT
+		artist_name
+	FROM staging.spotify_streams
+) ar;
 
