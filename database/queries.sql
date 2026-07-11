b504ee59ebf3ee1dc1c3656f6597694128cf9a72
Evan Nartea <evannartea@Mac.lan>
2026-07-11 19:11:34 +0100

    add fact table

--- database/queries.sql
+++ database/queries.sql
@@ -54,7 +54,7 @@ CREATE TABLE warehouse.dim_track (
 	artist_name VARCHAR(255),
 	album_name VARCHAR (255),
 
-	UNIQUE(
+	UNIQUE (
 		track_name,
 		artist_name,
 		album_name
@@ -81,11 +81,108 @@ CREATE TABLE warehouse.dim_country (
 	country_code VARCHAR(10) UNIQUE NOT NULL
 );
 
-INSERT INTO warehouse.dim_country(
+INSERT INTO warehouse.dim_country (
 	country_code
 )
 SELECT DISTINCT
 	country
 FROM staging.spotify_streams
 ORDER BY country;
+
+-- Create context dim
+DROP TABLE IF EXISTS warehouse.dim_play_info;
+
+CREATE TABLE warehouse.dim_play_info (
+	play_info_id SERIAL PRIMARY KEY,
+	reason_start VARCHAR(100),
+	reason_end VARCHAR(100),
+	shuffle BOOLEAN,
+	skipped BOOLEAN,
+
+	UNIQUE (
+		reason_start,
+		reason_end,
+		shuffle,
+		skipped
+	)
+);
+
+INSERT INTO warehouse.dim_play_info (
+	reason_start,
+	reason_end,
+	shuffle,
+	skipped
+)
+SELECT DISTINCT
+	reason_start,
+	reason_end,
+	shuffle,
+	skipped
+FROM staging.spotify_streams
+ORDER BY reason_start, reason_end;
+
+-- Create fact table
+DROP TABLE IF EXISTS warehouse.fact_streams;
+
+CREATE TABLE warehouse.fact_streams (
+	stream_id BIGSERIAL PRIMARY KEY,
+
+	date_id INT NOT NULL,
+	track_id INT NOT NULL,
+	country_id INT NOT NULL,
+	play_info_id INT NOT NULL,
+
+	milliseconds_played INT,
+	minutes_played NUMERIC(10,2),
+
+	FOREIGN KEY(date_id)
+		REFERENCES warehouse.dim_date(date_id),
+
+	FOREIGN KEY(track_id)
+		REFERENCES warehouse.dim_track(track_id),
+
+	FOREIGN KEY(country_id)
+		REFERENCES warehouse.dim_country(country_id),
+
+	FOREIGN KEY(play_info_id)
+		REFERENCES warehouse.dim_play_info(play_info_id)
+);
+
+INSERT INTO warehouse.fact_streams (
+	date_id,
+	track_id,
+	country_id,
+	play_info_id,
+
+	milliseconds_played,
+	minutes_played
+)
+
+SELECT
+	d.date_id,
+	t.track_id,
+	c.country_id,
+	pi.play_info_id,
+
+	s.milliseconds_played,
+	s.mins_played
+FROM staging.spotify_streams s
+
+JOIN warehouse.dim_date d
+	ON s.date_played::date = d.full_date
+
+JOIN warehouse.dim_track t
+    ON s.track_name = t.track_name
+    AND s.artist_name = t.artist_name
+    AND s.album_name = t.album_name
+
+JOIN warehouse.dim_country c
+    ON s.country = c.country_code
+
+JOIN warehouse.dim_play_info pi
+    ON s.reason_start = pi.reason_start
+    AND s.reason_end = pi.reason_end
+    AND s.shuffle = pi.shuffle
+    AND s.skipped = pi.skipped;
+
 
