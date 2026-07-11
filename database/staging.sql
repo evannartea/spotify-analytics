ec39383118566a2ec7d7fbe0dd92f0bbe8536a83
Evan Nartea <evannartea@Mac.lan>
2026-07-11 21:49:56 +0100

    separate sql queries

---
+++ database/staging.sql
@@ -0,0 +1,19 @@
+-- STAGING
+-- Filter raw data
+DROP TABLE IF EXISTS staging.spotify_streams;
+
+CREATE TABLE staging.spotify_streams AS
+SELECT 
+	ts AS date_played, 
+	ms_played AS milliseconds_played, 
+	conn_country AS country, 
+	master_metadata_track_name AS track_name, 
+	master_metadata_album_artist_name AS artist_name, 
+	master_metadata_album_album_name AS album_name, 
+	reason_start, 
+	reason_end, 
+	shuffle, 
+	skipped, 
+	mins_played
+FROM public.streaming_history
+ORDER BY ts ASC;
