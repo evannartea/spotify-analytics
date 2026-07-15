7db98c2dcabf1cc21c8ef7f85c677f5efe3ea1a2
Evan Nartea <evannartea@Mac.lan>
2026-07-16 00:26:57 +0100

    cuppla tweaks

--- functions/genre.py
+++ functions/genre.py
@@ -1,19 +1,9 @@
 import os
-import glob
 import time
 import requests
 import pandas as pd
+from functions.elt import load_data
 from dotenv import load_dotenv
-
-def load_data():
-    dataframes = []
-    files = glob.glob("data/raw/Streaming_History_Audio_*.json")
-
-    for file in files:
-        dataframes.append(pd.read_json(file))
-
-    return pd.concat(dataframes, ignore_index=True)
-
 
 def get_genres(artist_name, api_key, session):
     url = f"https://ws.audioscrobbler.com/2.0/"
@@ -40,34 +30,31 @@ def get_genres(artist_name, api_key, session):
 
     return result
 
-def main():
-    df = load_data()
 
-    load_dotenv()
-    api_key = os.getenv("API_KEY")
+df = load_data()
 
-    session = requests.Session()
-    artists = sorted(df["master_metadata_album_artist_name"].dropna().unique())
+load_dotenv()
+api_key = os.getenv("API_KEY")
 
-    rows = []
+session = requests.Session()
+artists = sorted(df["master_metadata_album_artist_name"].dropna().unique())
+
+rows = []
+
+for artist in artists:
+    genres = get_genres(artist, api_key, session)
+    time.sleep(0.2)
     
-    for artist in artists:
-        genres = get_genres(artist, api_key, session)
-        time.sleep(0.2)
-        
-        for genre in genres:
-            rows.append(
-                {
-                    "artist_name": artist,
-                    "genre": genre["name"],
-                    "weight": genre["count"]
-                }
-            )
+    for genre in genres:
+        rows.append(
+            {
+                "artist_name": artist,
+                "genre": genre["name"],
+                "weight": genre["count"]
+            }
+        )
 
-    df_genre = pd.DataFrame(rows)
-    df_genre.to_csv("data/raw/genres.csv", index=False)
+df_genre = pd.DataFrame(rows)
+df_genre.to_csv("data/raw/genres.csv", index=False)
 
-    print("Completed!")
-
-if __name__ == "__main__":
-    main()
+print("Completed!")
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from functions.elt import load_data, export_to_db, extract_from_db
@@ -8,20 +9,21 @@ def main():
     database_url = os.getenv("DATABASE_URL")
     engine = create_engine(database_url)
     
-    df = load_data()
+    df_streams = load_data()
+    df_genre = pd.read_csv("data/raw/genres.csv")
 
-    export_to_db("streaming history", df, engine)
+    export_to_db("artist_genres", df_genre, engine)
+    export_to_db("streaming_history", df_streams, engine)
 
     print("Exported successfully!")
 
-    """
     extract_from_db("dim_date", engine)
     extract_from_db("dim_time", engine)
     extract_from_db("dim_track", engine)
     extract_from_db("dim_country", engine)
     extract_from_db("dim_play_info", engine)
     extract_from_db("fact_streams", engine)
-    """
+    
     print("Extracted succesfully!")
 
 if __name__ == "__main__":
