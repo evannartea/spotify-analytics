2837b06fa06997273b85cde848863fa8517e3d83
Evan Nartea <evannartea@Mac.lan>
2026-07-16 11:18:45 +0100

    added alias

--- database/staging.sql
+++ database/staging.sql
@@ -19,22 +19,27 @@ FROM public.streaming_history
 WHERE master_metadata_track_name IS NOT NULL
 	AND master_metadata_album_artist_name IS NOT NULL
 	AND master_metadata_album_album_name IS NOT NULL
+	AND ts > '2022-01-01 00:00:00'
 ORDER BY ts ASC;
 
 -- Filter genres data
 DROP TABLE IF EXISTS staging.artist_genres;
 
 CREATE TABLE staging.artist_genres AS
-SELECT 
-	artist_name,
-    regexp_replace(
-		lower(
-			regexp_replace(genre, '[-_\s]', '', 'g')
-		),
-		'^(.*)$',
-		'\1'
-	) AS normalised_genre,
-	weight
-FROM public.artist_genres
-WHERE weight > 50
-ORDER BY artist_name ASC;
+SELECT
+    t.artist_name,
+    t.normalised_genre,
+    t.weight
+FROM (
+    SELECT
+        artist_name,
+        lower(regexp_replace(genre, '[-_\s]', '', 'g')) AS normalised_genre,
+        weight,
+        COUNT(*) OVER (
+            PARTITION BY lower(regexp_replace(genre, '[-_\s]', '', 'g'))
+        ) AS genre_count
+    FROM public.artist_genres
+    WHERE weight > 50
+) t
+WHERE t.genre_count >= 30
+ORDER BY t.artist_name ASC;
